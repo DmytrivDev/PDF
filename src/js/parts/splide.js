1544f270ddf4856f7fa0aa8f8faf9feb620976fe
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-01-31 15:26:47 +0200

    Initial commit

---
+++ src/js/parts/splide.js
@@ -0,0 +1,55 @@
+import { initSlider } from './splidecust';
+
+// const elemSplide = document.querySelector('.elem');
+// if (elemSplide) {
+//   initSlider(elemSplide, {
+//     perPage: 2,
+//     breakpoints: {
+//       960: {},
+//       500: {},
+//     },
+//   });
+// }
+
+// const elemSplides = document.querySelectorAll('.elem');
+// elemSplides?.forEach(container => {
+//   initSlider(container, {
+//     perPage: 2,
+//     breakpoints: {
+//       960: {},
+//       500: {},
+//     },
+//   });
+// });
+
+// let elemSliderInstance;
+// const elem = document.querySelector('.elem');
+
+// const initOurproductSlider = () => {
+//   if (elem && !elemSliderInstance) {
+//     elemSliderInstance = initSlider(elem, {
+//       perPage: 2,
+//       breakpoints: {
+//         960: {},
+//         500: {},
+//       },
+//     });
+//   }
+// };
+
+// const destroySliders = () => {
+//   if (elemSliderInstance) {
+//     elemSliderInstance.destroy();
+//     elemSliderInstance = null;
+//   }
+// };
+
+// const checkViewport = () => {
+//   initOurproductSlider();
+//   if (window.innerWidth > 960) {
+//     destroySliders();
+//   }
+// };
+
+// window.addEventListener('resize', checkViewport);
+// document.addEventListener('DOMContentLoaded', checkViewport);
