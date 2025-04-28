8cdf4426f4cf5b0a0501240395eceac953353574
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-04-28 18:03:34 +0300

    фівфі

--- src/js/parts/exchangerates.js
+++ src/js/parts/exchangerates.js
@@ -5,6 +5,7 @@ const localRatesStore = {
       rates: {
         'USD-W_UAH': { buy: 40.85, sell: 41.0 },
         'USD-B_UAH': { buy: 40.84, sell: 40.9 },
+        'USD-W_USD-B': { buy: 1.1, sell: 1.2 },
         'EUR_UAH': { buy: 45.1, sell: 45.5 },
         'GBP_UAH': { buy: 52.0, sell: 53.5 },
         'EUR_USD-W': { buy: 1.101, sell: 1.119 },
@@ -16,6 +17,7 @@ const localRatesStore = {
       rates: {
         'USD-W_UAH': { buy: 40.95, sell: 41.1 },
         'USD-B_UAH': { buy: 40.95, sell: 41.2 },
+        'USD-W_USD-B': { buy: 1.2, sell: 1.3 },
         'EUR_UAH': { buy: 45.2, sell: 45.6 },
         'GBP_UAH': { buy: 52.1, sell: 53.6 },
         'EUR_USD-W': { buy: 1.201, sell: 1.219 },
@@ -27,6 +29,7 @@ const localRatesStore = {
       rates: {
         'USD-W_UAH': { buy: 41.05, sell: 41.2 },
         'USD-B_UAH': { buy: 41.05, sell: 41.3 },
+        'USD-W_USD-B': { buy: 1.3, sell: 1.4 },
         'EUR_UAH': { buy: 45.3, sell: 45.7 },
         'GBP_UAH': { buy: 52.2, sell: 53.7 },
         'EUR_USD-W': { buy: 1.301, sell: 1.319 },
