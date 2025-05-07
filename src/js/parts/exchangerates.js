36c804f1d155ee37a7ba15ac68e5224014eae7c3
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-05-07 17:48:21 +0300

    sdfsdf

--- src/js/parts/exchangerates.js
+++ src/js/parts/exchangerates.js
@@ -4,8 +4,6 @@ const localRatesStore = {
       maxAmount: 1000,
       rates: {
         'USD-W_UAH': { buy: 40.85, sell: 41.0 },
-        'USD-B_UAH': { buy: 40.84, sell: 40.9 },
-        'USD-W_USD-B': { buy: 1.1, sell: 1.2 },
         EUR_UAH: { buy: 45.1, sell: 45.5 },
         GBP_UAH: { buy: 52.0, sell: 53.5 },
         'EUR_USD-W': { buy: 1.101, sell: 1.119 },
@@ -16,8 +14,6 @@ const localRatesStore = {
       maxAmount: 5000,
       rates: {
         'USD-W_UAH': { buy: 40.95, sell: 41.1 },
-        'USD-B_UAH': { buy: 40.95, sell: 41.2 },
-        'USD-W_USD-B': { buy: 1.2, sell: 1.3 },
         EUR_UAH: { buy: 45.2, sell: 45.6 },
         GBP_UAH: { buy: 52.1, sell: 53.6 },
         'EUR_USD-W': { buy: 1.201, sell: 1.219 },
@@ -28,8 +24,6 @@ const localRatesStore = {
       maxAmount: Infinity,
       rates: {
         'USD-W_UAH': { buy: 41.05, sell: 41.2 },
-        'USD-B_UAH': { buy: 41.05, sell: 41.3 },
-        'USD-W_USD-B': { buy: 1.3, sell: 1.4 },
         EUR_UAH: { buy: 45.3, sell: 45.7 },
         GBP_UAH: { buy: 52.2, sell: 53.7 },
         'EUR_USD-W': { buy: 1.301, sell: 1.319 },
@@ -43,7 +37,6 @@ const localRatesStore = {
       maxAmount: 1000,
       rates: {
         'USD-W': { buy: 1.014, sell: 1.018 },
-        'USD-B': { buy: 1.011, sell: 1.014 },
         EUR: 'same_usd',
         GBP: 'same_usd',
         UAH: 'same_usd',
@@ -53,7 +46,6 @@ const localRatesStore = {
       maxAmount: 10000,
       rates: {
         'USD-W': { buy: 1.015, sell: 1.019 },
-        'USD-B': { buy: 1.012, sell: 1.015 },
         EUR: 'cross',
         GBP: 'cross',
         UAH: 'cross',
@@ -63,7 +55,6 @@ const localRatesStore = {
       maxAmount: 50000,
       rates: {
         'USD-W': { buy: 1.016, sell: 1.02 },
-        'USD-B': { buy: 1.013, sell: 1.016 },
         EUR: 'cross',
         GBP: 'cross',
         UAH: 'cross',
@@ -73,7 +64,6 @@ const localRatesStore = {
       maxAmount: Infinity,
       rates: {
         'USD-W': { buy: 1.017, sell: 1.021 },
-        'USD-B': { buy: 1.014, sell: 1.017 },
         EUR: 'cross',
         GBP: 'cross',
         UAH: 'cross',
